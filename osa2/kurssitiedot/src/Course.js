84b4799161dcc02e31dddd770ddc197a90d328a8
Veeti Laine <veeti.laine@tuni.fi>
2023-01-06 21:29:11 +0200

    2.1-2.5 kurssitiedot

---
+++ osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,23 @@
+const Course = ({course}) => (
+    <>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+    </>
+);
+
+const Header = ({name}) => (<h1>{name}</h1>);
+
+const Content = ({parts}) => (
+    <div>
+        {parts.map(part => (
+            <Part key={part.id} part={part}/>
+        ))}
+    </div>
+);
+
+const Part = ({part}) => (<p>{part.name} {part.exercises}</p>);
+
+const Total = ({parts}) => (<p>total of {parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises</p>);
+
+export default Course;
