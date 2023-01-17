e8f1ffc134be3bad80e8f54ee116e9e65776adeb
Veeti Laine <veeti.laine@tuni.fi>
2023-01-17 18:19:40 +0200

    bloglist refactored into modules

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { PORT, MONGODB_URI } = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+const { info } = require('./utils/logger');
 const mongoose = require('mongoose');
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
+mongoose.connect(MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/blogs', blogsRouter);
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs);
-        });
-});
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        });
-});
-
-const PORT = 3003;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    info(`Server running on port ${PORT}`);
 });
