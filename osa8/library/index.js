5a1f8aabfbc0909d9b1ff7298b49001d5852e020
Veeti <veeti.laine@tuni.fi>
2024-03-29 15:24:56 +0200

    part 8 refactoring backend

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -1,9 +1,19 @@
 const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const {
+    ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+
 const User = require('./models/User');
 const resolvers = require('./api/resolver');
-const jwt = require('jsonwebtoken');
+const typeDefs = require('./schema');
+
 require('dotenv').config();
 
 mongoose
@@ -15,72 +25,43 @@ mongoose
         console.log('error connecting to mongoDB', error.message);
     });
 
-const typeDefs = `
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(author: String, genre: String): [Book!]!
-    allAuthors: [Author!]!
-    me: User
-  }
+const start = async () => {
+    const app = express();
+    const httpServer = http.createServer(app);
 
-  type Mutation {
-    addBook(title: String!, author: String!, published: Int!, genres: [String!]!): Book!
-    editAuthor(name: String!, setBornTo: Int!): Author
-    createUser(
-        username: String!
-        favoriteGenre: String!
-    ): User
-        login(
-        username: String!
-        password: String!
-    ): Token
-  }
+    const server = new ApolloServer({
+        schema: makeExecutableSchema({ typeDefs, resolvers }),
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
 
-  type Author {
-    name: String!
-    born: Int
-    id: ID!
-    bookCount: Int!
-  }
+    await server.start();
 
-  type Book {
-    title: String!
-    published: Int!
-    author: Author!
-    id: ID!
-    genres: [String!]!
-  }
+    app.use(
+        '/',
+        cors(),
+        express.json(),
+        expressMiddleware(server, {
+            context: async ({ req }) => {
+                const auth = req ? req.headers.authorization : null;
+                if (auth && auth.startsWith('Bearer ')) {
+                    const decodedToken = jwt.verify(
+                        auth.substring(7),
+                        process.env(SECRET)
+                    );
+                    console.log('user:' + decodedToken);
+                    const currentUser = await User.findById(decodedToken.id);
 
-  type User {
-    username: String!
-    favoriteGenre: String!
-    id: ID!
-  }
+                    return { currentUser };
+                }
+                return null;
+            },
+        })
+    );
 
-  type Token {
-    value: String!
-  }
-`;
+    const PORT = 4000;
+    httpServer.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+};
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
-
-startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req, res }) => {
-        const auth = req ? req.headers.authorization : null;
-        if (auth && auth.startsWith('Bearer ')) {
-            const decodedToken = jwt.verify(
-                auth.substring(7),
-                process.env.SECRET
-            );
-            const currentUser = await User.findById(decodedToken.id);
-            return { currentUser };
-        }
-    },
-}).then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-});
+start();
