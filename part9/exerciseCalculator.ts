4df00179635fc7b863b61bfbdf8d72fd8ae65acf
Veeti <veeti.laine@tuni.fi>
2024-04-02 11:12:39 +0300

    part9/9.6-9.7

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,12 +1,4 @@
-interface Result {
-    periodLength: number;
-    trainingDays: number;
-    success: boolean;
-    rating: number;
-    ratingDescription: string;
-    target: number;
-    average: number;
-}
+import { calculateExercises } from './calculateExercises';
 
 interface Arguments {
     target: number;
@@ -22,27 +14,6 @@ const parseArguments = (args: string[]): Arguments => {
     } else {
         return { target, hours };
     }
-};
-
-const calculateExercises = (hours: number[], target: number): Result => {
-    const average = hours.reduce((s, h) => s + h, 0) / hours.length;
-    const avgOfTarget = average / target;
-    const rating = avgOfTarget >= 2 ? 3 : avgOfTarget >= 1 ? 2 : 1;
-    const ratingDescription =
-        rating === 3
-            ? 'well done, exceeded target greatly'
-            : rating === 2
-            ? 'well done'
-            : 'goal not met';
-    return {
-        periodLength: hours.length,
-        trainingDays: hours.filter((h) => h > 0).length,
-        success: rating >= 2,
-        rating,
-        ratingDescription,
-        target,
-        average,
-    };
 };
 
 try {
--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import calculateBmi from './calculateBmi';
+import { calculateExercises } from './calculateExercises';
 
 const app = express();
+app.use(express.json());
+
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
 });
@@ -19,6 +22,20 @@ app.get('/bmi', (req, res) => {
     } catch (error: unknown) {
         const errorMsg = { error: 'malformatted parameters' };
         res.json(errorMsg);
+    }
+});
+app.post('/exercises', (req, res) => {
+    const input = req.body;
+    console.log(input);
+    try {
+        const hours = input.daily_exercises.map((h: any) => Number(h));
+        const target = Number(input.target);
+        if (hours.length < 1 || hours.concat(target).some(isNaN)) {
+            res.json({ error: 'malformatted parameters' });
+        }
+        res.json(calculateExercises(hours, target));
+    } catch (error: unknown) {
+        res.json({ error: 'parameters missing' });
     }
 });
 
